bbc8eda0745566f534810ee7da652b12b046ff0a
OmerJauhar <p218055@pwr.nu.edu.pk>
2024-08-31 17:19:28 +0500

    technolgies updated

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import kws from '../images/clients/kws.png';
-import geps from '../images/clients/geps.png';
-import protergia from '../images/clients/protergia.png';
+import typescript from '../images/clients/typescript.svg';
+import react from '../images/clients/react-2.svg';
+import protergia from '../images/clients/nodejs.svg';
+import spring from '../images/clients/spring-3.svg';
+import pytorch from '../images/clients/pytorch-2.svg';
+import pythonlogo from '../images/clients/python-5.svg'
+
 
 const clientImage = {
     height: '10rem',
@@ -16,15 +20,15 @@ const Clients = () => {
                 <div className="my-4 py-4">
                         <h2 className="my-2 text-center text-3xl text-black uppercase font-bold">Technologies </h2>
                         <div className='flex justify-center'>
-                            <div className='w-24 border-b-4 border-blue-900'></div>
+                            <div className='w-24 border-b-4 border-black'></div>
                         </div>
-                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-black">Some of our clients.</h2>
+                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-black">Some of technologies used.</h2>
                     </div>
 
                 <div className="p-16" data-aos="fade-in" data-aos-delay="600">
                     <div className="grid sm:grid-cols-3 lg:grid-cols-3">
                         <div style={clientImage} className= "overflow-hidden flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100 w-1/6">
-                            <img src={kws} alt="client" />                           
+                            <img src={typescript} alt="client" />                           
                         </div>
 
                         <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
@@ -32,19 +36,19 @@ const Clients = () => {
                         </div> 
 
                         <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={geps} alt="client" />                            
+                            <img src={pytorch} alt="client" />                            
                         </div>
 
                         <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={geps} alt="client" />                            
+                            <img src={spring} alt="client" />                            
                         </div>
 
                         <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={geps} alt="client" />                            
+                            <img src={react} alt="client" />                            
                         </div>
                         
                         <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={geps} alt="client" />                            
+                            <img src={pythonlogo} alt="client" />                            
                         </div>
 
                                            
