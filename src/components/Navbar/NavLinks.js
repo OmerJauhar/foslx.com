5dd703d938c908b336dec9666be0870fe41fcb53
OmerJauhar <p218055@pwr.nu.edu.pk>
2024-08-31 17:35:30 +0500

    contact us update

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -10,7 +10,7 @@ const NavLinks = () => {
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-black" smooth to="/#services">
                 Services
             </HashLink>
-            <HashLink className="text-white bg-black hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" to="/contact#contact">
+            <HashLink className="text-white bg-black hover:bg-gray-500 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" to="/contact#contact">
                 Contact Us
             </HashLink>
         </>
